25b735c6cc682caaa3cca240c7061e277f28710b
JamesWrigley <james@puiterwijk.org>
2024-08-13 17:52:29 +0200

    Regenerate the bindings with comments and save the metadata
    We need to do this separately because upstream cimgui doesn't include comments
    by default.

--- build.jl
+++ build.jl
@@ -5,6 +5,24 @@ import GLFW_jll
 
 cxxwrap_prefix = CxxWrap.prefix_path()
 
+output_dir = joinpath(@__DIR__, "cimgui_comments_output")
+if !isdir(output_dir)
+    mkdir(output_dir)
+end
+
+# Regenerate the cimgui bindings with comments enabled
+cd(joinpath(@__DIR__, "cimgui/generator")) do
+    script = read("generator.sh", String)
+    script = replace(script, "TARGETS=\"" => "TARGETS=\"comments ")
+    write("generator_with_comments.sh", script)
+    chmod("generator_with_comments.sh", 0o744)
+    run(`./generator_with_comments.sh`)
+
+    for json_file in filter(endswith(".json"), readdir("output"; join=true))
+        cp(json_file, joinpath(output_dir, basename(json_file)); force=true)
+    end
+end
+
 # Generate the imgui_test_engine wrappers
 cd(joinpath(@__DIR__, "test_engine")) do
     # Write the config file from the template
